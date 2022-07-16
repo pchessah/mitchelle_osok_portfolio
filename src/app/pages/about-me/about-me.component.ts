345066f4e2717dd192d639e6214d44a981435624
Paul Chesa <paulchesa1@gmail.com>
2022-07-16 12:48:57 +0300

    Fix css on about us page

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
@@ -6,8 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['about-me.component.scss']
 })
 
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent implements OnDestroy, OnInit {
+  isContentOpen: boolean = false;
+  isCollapsed = true;
+  focus;
+  focus1;
+  focus2;
+  date = new Date();
+  pagination = 3;
+  pagination1 = 1;
+
   constructor() { }
 
-  ngOnInit() { }
+  scrollToDownload(element: any) {
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+  ngOnInit() {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.add("index-page");
+  }
+
+  toggleAccordion(){
+    this.isContentOpen = !this.isContentOpen;
+  }
+
+  ngOnDestroy() {
+    var body = document.getElementsByTagName("body")[0];
+    body.classList.remove("index-page");
+  }
 }
